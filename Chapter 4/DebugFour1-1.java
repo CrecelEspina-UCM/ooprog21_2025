76a19e9c9abc62cc630dc4aa6a1af62a9658673a
CrecelEspina-UCM <crecelespina15@gmail.com>
2025-09-30 16:04:35 +0800

    Update DebugFour1-1.java

--- Chapter 4/DebugFour1-1.java
+++ Chapter 4/DebugFour1-1.java
@@ -1,34 +1,19 @@
 import java.util.Scanner;
 
-public class DebugFour2
+public class DebugFour1
 {
    public static void main(String[] args)
    {
       Scanner input = new Scanner(System.in);
-      String color;
-      String point;
-      DebugPen pen1;
-      DebugPen pen2;
+      int radius;
 
-      System.out.print("Enter a color ink for the pen >> ");
-      color = input.nextLine();
+      System.out.print("Enter a radius for a circle >> ");
+      radius = input.nextInt();
 
-      System.out.print("Enter a point size - fine, medium, or thick >> ");
-      point = input.nextLine();
+      DebugCircle c = new DebugCircle(radius);
 
-      pen1 = new DebugPen(); 
-      pen2 = new DebugPen(color, point); 
-
-      System.out.println("Default value pen: ");
-      display(pen1);
-
-      System.out.println("User value pen: ");
-      display(pen2);
-   }
-
-   public static void display(DebugPen p)
-   {
-      System.out.println("   The pen has ink color " + p.getColor());
-      System.out.println("      and a " + p.getPoint() + " point.");
+      System.out.println("The radius is " + c.getRadius());
+      System.out.println("The diameter is " + c.getDiameter());
+      System.out.println("The area is " + c.getArea());
    }
 }
