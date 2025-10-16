6d9ba248f86d2e248614f78f69ea2540a68ee440
CrecelEspina-UCM <crecelespina15@gmail.com>
2025-10-16 11:15:52 +0800

    Update Department.java

--- Midterm_/Department.java
+++ Midterm_/Department.java
@@ -1,17 +1,23 @@
 public class Department {
-    private int code;
     private String name;
+    private int code;
 
-    public Department() { }
-
-    public Department(int code, String name) {
-        this.code = code;
-        this.name = name;
+    public Department() {
+        this.code = 1;
+        this.name = "Software Engineer Department";
     }
 
-    public int getCode() { return code; }
-    public void setCode(int code) { this.code = code; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-}
+    public void setDepartment(int code) {
+        this.code = code;
+        switch (code) {
+            case 1 -> this.name = "Software Engineer Department";
+            case 2 -> this.name = "Software Design Department";
+            case 3 -> this.name = "Customer Experience Department";
+            case 4 -> this.name = "Software Quality Assurance Department";
+            default -> this.name = "Unknown Department";
+        }
+    }
 
+    public String getDepartmentName() { return name; }
+    public int getDepartmentCode() { return code; }
+}
