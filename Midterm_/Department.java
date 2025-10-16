fcccfe8500b43f479e850e2d01f3f00c56d69780
CrecelEspina-UCM <crecelespina15@gmail.com>
2025-10-16 11:00:06 +0800

    Update Department.java

--- Midterm_/Department.java
+++ Midterm_/Department.java
@@ -1,1 +1,17 @@
+public class Department {
+    private int code;
+    private String name;
+
+    public Department() { }
+
+    public Department(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public int getCode() { return code; }
+    public void setCode(int code) { this.code = code; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
 
